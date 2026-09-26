ed698aeddc6e00386ee3d57c9e4255b072857b28
GeoMasX <z.gr.o2008@gmail.com>
2026-09-26 09:46:26 +0300

    directory_tree_manager 3/3
    Сделал откат крч, чтобы нахрен удалить вам win32

--- venv/src/venv/directory_tree_maker.py
+++ venv/src/venv/directory_tree_maker.py
@@ -20,13 +20,21 @@ def push_new_file(old_path, new_path):
         currect_path += '/' + next_folder
     os.replace(old_path, new_path)
 
+def move_files_into_root(main_root, path):
+    for next_file in os.listdir(path):
+        next_path = path + '/' + next_file
+        if os.path.isdir(next_path):
+            move_files_into_root(main_root, next_path)
+        else:
+            os.replace(next_path, main_root + '/' + next_file)
+
 def clear(path):
     for next_file in os.listdir(path):
         next_path = path + '/' + next_file
         if os.path.isdir(next_path):
-            saved_file_paths = clear(next_path)
-            if len(os.listdir(path)) == 0:
-                os.rmdir(next_path)
+            clear(next_path)
+    if len(os.listdir(path)) == 0:
+        os.rmdir(path)
                 
 def directory_tree_maker_manager(main_root, new_paths, programm_path):
     saved_file_paths = []
@@ -34,15 +42,27 @@ def directory_tree_maker_manager(main_root, new_paths, programm_path):
     
     data = {}
     data["saved_file_paths"] = saved_file_paths
-    with open(programm_path + "/saved_paths.json", "w") as f:
-        json.dump(data, f)
+    with open(programm_path + "/saved_paths.json", "w") as json_data:
+        json.dump(data, json_data)
+    json_data.close()
     
     for paths_pair in new_paths:
         push_new_file(paths_pair[0], paths_pair[1])
 
     clear(main_root)
     
-#def rollback(programm_path):
+def rollback(main_root, programm_path):
+    with open(programm_path + "/saved_paths.json", "r") as json_data:
+        data = json.load(json_data)
+    json_data.close()
+    saved_file_paths = data["saved_file_paths"]
     
-
+    move_files_into_root(main_root, main_root)
+    
+    for path in saved_file_paths:
+        push_new_file(main_root + '/' + path.split('/')[-1], path)
+    
+    clear(main_root)
+    
 directory_tree_maker_manager("C:/Users/MSI/Downloads/gg2", [["C:/Users/MSI/Downloads/gg2/vf.txt", "C:/Users/MSI/Downloads/gg2/new/vf.txt"]], "C:/Users/MSI/Downloads/project_shlak")
+rollback("C:/Users/MSI/Downloads/gg2", "C:/Users/MSI/Downloads/project_shlak")
